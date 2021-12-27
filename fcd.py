6ce1233c7ae864599445681c8148d7c14d731cea
Derek Kowaluk <derek.kowaluk@bentley.com>
2021-12-27 14:54:15 -0700

    Switch to recursive

--- fcd.py
+++ fcd.py
@@ -12,13 +12,12 @@ def print_usage():
 	print("Usage:")
 
 
-def scan_with_tags(taglist, dirpath = '.'):
-	#find all folders, compare to taglist if found, cd and continue
-	#print(taglist)
+def find_forward(taglist, dirpath = '.'):
+	if not len(taglist):
+		return (True, dirpath)
 
+	result = (False, dirpath)
 	s_it = os.scandir(dirpath)
-	trylist = []
-	foundlist = []
 	with s_it:
 		while True:
 			try:
@@ -29,39 +28,22 @@ def scan_with_tags(taglist, dirpath = '.'):
 			except OSError as error:
 				if onerror is not None:
 					onerror(error)
-				return
+				return result
 			try:
 				is_dir = entry.is_dir()
 			except OSError:
 				is_dir = False
-
 			if is_dir:
 				basename = os.path.basename(entry.name)
 				if basename[0] == '.': continue
 				if basename in taglist:
-					foundlist.append((basename, os.path.abspath(entry.path)))
-					print(os.path.abspath(entry.path))
-				else :
-					trylist.append(os.path.abspath(entry.path))
-		return (foundlist,trylist)
-
-
-
-def change_with_tags(taglist, dirpath = '.'):
-
-	foundlist, trylist = scan_with_tags(taglist, dirpath)
-
-	while True:
-
-		if (foundlist) :
-			for eachone in foundlist:
-				mytags = taglist.copy()
-				mytags.remove(eachone[0])
-				f, t = scan_with_tags(mytags, eachone[1])
-			print("found some")
-		else:
-			print(trylist)
-
+					nextlist = taglist.copy()
+					nextlist.remove(basename)
+					result = find_forward(nextlist, os.path.abspath(entry.path))
+				else:
+					result = find_forward(taglist, os.path.abspath(entry.path))
+				if (result[0] == True):
+					return result
 
 
 
@@ -72,7 +54,11 @@ def main():
 		print_usage()
 		sys.exit()
 
-	change_with_tags(sys.argv[1:])
+	result = find_forward(sys.argv[1:])
+	if result[0]:
+		print(result)
+	else:
+		print("Not Found")
 
 if __name__ == '__main__':
 	main()
