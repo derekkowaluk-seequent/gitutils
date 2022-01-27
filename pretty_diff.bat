a46b977a2fa1ed15f5e0206afd67a034001cb6dd
Derek Kowaluk <derek.kowaluk@bentley.com>
2022-01-26 23:57:27 -0700

    rename utilities

--- pretty_diff.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call setup_colors.bat
-
-chgcolor %_CLR_INFO%
-for /f "delims==" %%a in ('git diff --name-only origin/main...HEAD') do git log -n1 --pretty=format:"%%Cblue%%h%%Creset %%Cgreen(%%cr)%%Creset -%%C(yellow)%%d%%Creset %%s" %%a
-chgcolor %_CLR_ORIG%
